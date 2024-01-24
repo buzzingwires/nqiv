6b6242751ce673d892811232de98e2f292089c77
buzzingwires <buzzingwires@outlook.com>
2024-01-23 21:01:14 -0500

    More array methods and minor cleanup.

--- src/array.c
+++ src/array.c
@@ -15,7 +15,6 @@ nqiv_array* nqiv_array_create(const int starting_length)
 		return array;
 	}
 	array->data_length = starting_length;
-	array->position = 0;
 	return array;
 }
 
@@ -40,7 +39,7 @@ bool nqiv_array_grow(nqiv_array* array, const int new_length)
 	return true;
 }
 
-bool nqiv_array_push_ptr(nqiv_array* array, void* ptr)
+bool nqiv_array_make_room(nqiv_array* array, const int size)
 {
 	if(array == NULL) {
 		return false;
@@ -52,8 +51,47 @@ bool nqiv_array_push_ptr(nqiv_array* array, void* ptr)
 	if( array->data_length < minimum_length && !nqiv_array_grow(array, minimum_length) ) {
 		return false;
 	}
-	*((void**)(array->data[array->position])) = ptr;
+	return true;
+}
+
+bool nqiv_array_insert_ptr(nqiv_array* array, void* ptr, const int idx)
+{
+	if( !nqiv_array_make_room( array, sizeof(void*) ) ) {
+		return false;
+	}
+	const int offset = idx * sizeof(void*);
+	const int next_offset = offset + sizeof(void*);
+	const int final_offset = array->data_length - sizeof(void*);
+	assert(next_offset <= final_offset);
+	assert(array->position <= final_offset);
+	if(next_offset < final_offset) {
+		memcpy(&array->data[next_offset], &array->data[offset], array->position - offset)
+	}
+	*((void**)(array->data[offset])) = ptr;
 	array->position += sizeof(void*);
+	assert(array->position <= final_offset);
+	return true;
+}
+
+void nqiv_array_remove_ptr(nqiv_array* array, const int idx)
+{
+	const int offset = idx * sizeof(void*);
+	const int next_offset = offset + sizeof(void*);
+	const int final_offset = array->data_length - sizeof(void*);
+	assert(next_offset <= final_offset);
+	assert(array->position <= final_offset);
+	if(next_offset < final_offset) {
+		memcpy(&array->data[offset], &array->data[next_offset], array->position - next_offset)
+	}
+	array->position -= sizeof(void*);
+	assert(array->position <= final_offset);
+	assert(array->position >= 0);
+}
+
+bool nqiv_array_push_ptr(nqiv_array* array, void* ptr)
+{
+	assert(array->position % sizeof(void*) == 0);
+	return nqiv_array_insert_ptr( array, ptr, array->position / sizeof(void*) );
 }
 
 void* nqiv_array_get_ptr(nqiv_array* array, const int idx)
@@ -68,7 +106,7 @@ void* nqiv_array_get_ptr(nqiv_array* array, const int idx)
 		return NULL;
 	}
 	const int offset = idx * sizeof(void*);
-	if(offset > array->data_length) {
+	if( offset >= array->data_length - sizeof(void*) ) {
 		return NULL;
 	}
 	return ((void**)array->data)[offset];
@@ -92,6 +130,16 @@ void* nqiv_array_pop_ptr(nqiv_array* array)
 	return output;
 }
 
+bool nqiv_array_insert_char_ptr(nqiv_array* array, char* ptr, const int idx)
+{
+	return nqiv_array_insert_ptr(array, (void*)ptr, idx);
+}
+
+void nqiv_array_remove_char_ptr(nqiv_array* array, const int idx)
+{
+	nqiv_array_remove_ptr(array, idx);
+}
+
 bool nqiv_array_push_char_ptr(nqiv_array* array, char* ptr)
 {
 	return nqiv_array_push_ptr( array, (void*)ptr );
@@ -105,6 +153,16 @@ char* nqiv_array_get_char_ptr(nqiv_array* array, const int idx)
 void* nqiv_array_pop_char_ptr(nqiv_array* array)
 {
 	return (char*)nqiv_array_pop_ptr(array);
+}
+
+bool nqiv_array_insert_FILE_ptr(nqiv_array* array, FILE* ptr, const int idx)
+{
+	return nqiv_array_insert_ptr(array, (void*)ptr, idx);
+}
+
+void nqiv_array_remove_FILE_ptr(nqiv_array* array, const int idx)
+{
+	nqiv_array_remove_ptr(array, idx);
 }
 
 bool nqiv_array_push_FILE_ptr(nqiv_array* array, FILE* ptr)
--- src/array.h
+++ src/array.h
@@ -12,12 +12,19 @@ typedef struct nqiv_array
 
 nqiv_array* nqiv_array_create(const int starting_length);
 bool nqiv_array_grow(nqiv_array* array, const int new_length);
+bool nqiv_array_make_room(nqiv_array* array, const int size);
+bool nqiv_array_insert_ptr(nqiv_array* array, void* ptr, const int idx);
+void nqiv_array_remove_ptr(nqiv_array* array, const int idx);
 bool nqiv_array_push_ptr(nqiv_array* array, void* ptr);
 void* nqiv_array_get_ptr(nqiv_array* array, const int idx);
 void* nqiv_array_pop_ptr(nqiv_array* array);
+bool nqiv_array_insert_char_ptr(nqiv_array* array, char* ptr, const int idx);
+void nqiv_array_remove_char_ptr(nqiv_array* array, const int idx);
 bool nqiv_array_push_char_ptr(nqiv_array* array, char* ptr);
 char* nqiv_array_get_char_ptr(nqiv_array* array, const int idx);
 char* nqiv_array_pop_char_ptr(nqiv_array* array);
+bool nqiv_array_insert_FILE_ptr(nqiv_array* array, FILE* ptr, const int idx);
+void nqiv_array_remove_FILE_ptr(nqiv_array* array, const int idx);
 bool nqiv_array_push_FILE_ptr(nqiv_array* array, FILE* ptr);
 FILE* nqiv_array_get_FILE_ptr(nqiv_array* array, const int idx);
 FILE* nqiv_array_pop_FILE_ptr(nqiv_array* array);
