f7237992b217983d77080e925b08be597f246455
buzzingwires <buzzingwires@outlook.com>
2024-09-27 02:35:09 -0400

    Get make_appimage.sh working.
    * Make sure the architecture match doesn't accidentally match x86 against x86_64
    * Make the install option actually copy nqiv to the bindir.
    * Move the desktop file to scripts.
    * Set up infrastructure to generation the icon.
    * Generate the icon and specify a descriptive name for it.
    * Remember to set linuxdeploy as executable.
    * We aren't making the AppImage with tests. Don't both with an AppDir
      directory there.

--- scripts/make_appimage.sh
+++ scripts/make_appimage.sh
@@ -5,8 +5,8 @@ errcho()
 	>&2 echo "$@";
 }
 
-G_ARCH="(uname -m)"
-if [ "$(echo "$G_ARCH" | sed --quiet --posix --sandbox -E -e 's/(x86|i[3-9]86)/\1/p')" = "$G_ARCH" ]
+G_ARCH="$(uname -m)"
+if [ "$(echo "$G_ARCH" | sed --quiet --posix --sandbox -E -e 's/^(x86|i[3-9]86)$/\1/p')" = "$G_ARCH" ]
 then
 	G_ARCH="i386"
 elif [ "$G_ARCH" != "x86_64" ]
@@ -21,12 +21,13 @@ G_LINUXDEPLOY_LINK="https://github.com/linuxdeploy/linuxdeploy/releases/download
 if [ ! -e "./$G_LINUXDEPLOY" ]
 then
 	curl -L "$G_LINUXDEPLOY_LINK" -O 
+	chmod +x "$G_LINUXDEPLOY"
 fi
 
-rm -rf ./test/AppDir/ ./src/AppDir/;
-make install DESTDIR=AppDir;
+rm -rf ./src/AppDir/
+make install DESTDIR=./src/AppDir
 "./$G_LINUXDEPLOY"  --appdir=./src/AppDir/                  \
 					--executable=./src/AppDir/usr/bin/nqiv  \
-					--icon-file ./media/logo_N.png        \
-					--desktop-file ./scripts/nqiv.desktop \
+					--icon-file ./media/nqiv_icon_48x48.png \
+					--desktop-file ./scripts/nqiv.desktop   \
 					--output appimage
---
+++ scripts/show_icon.sh
@@ -0,0 +1,3 @@
+#!/bin/sh -efu
+
+src/nqiv -N -B -C './scripts/icon.cfg' './media/logo_N.png'
