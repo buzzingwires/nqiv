cba58d8da835e54122cbd4b9ef84e601e31d7a23
buzzingwires <buzzingwires@outlook.com>
2025-02-03 19:21:56 -0500

    Add `-l` and `-q` to nqivf.sh to print image list commands to file and to quit instead of running nqiv.

--- scripts/nqivf.sh
+++ scripts/nqivf.sh
@@ -35,6 +35,10 @@ exit_usage()
 	errcho
 	errcho "'-n' : Do not do automatic name sort. This alone will sort by natural order."
 	errcho
+	errcho "'-l' : Print the commands list passed to nqiv to the given path."
+	errcho
+	errcho "'-q' : Quit instead of running nqiv- useful with -l"
+	errcho
 	errcho "Passed through nqiv options:"
 	errcho
 	errcho "$(nqiv -h 2>&1 | sed --quiet --posix --sandbox -E -e '/^-h.+$/d' -e '/^-v.+$/d' -e '/^-s.+$/d' -e '/^To create default config.+$/d' -e 'p')"
@@ -54,6 +58,21 @@ check_file()
 	[ -f "$L_PATH" ]
 }
 
+populate_file()
+{
+	L_TARGETS="$1"
+	L_DEPTH="$2"
+	L_EXTENSIONS="$3"
+	L_FILTER="$4"
+	L_SORT="$5"
+	L_FILELIST_PATH="$6"
+
+	eval find $L_TARGETS "$L_DEPTH" "$L_EXTENSIONS" "$L_FILTER" -type f -printf "%T@\\\t%f\\\t%p\\\n" \
+	| eval "$L_SORT" \
+	| sed --quiet --posix --sandbox -E -e 's/^.+\t.+\t(.+)$/append image \1/' -e 'p' \
+	> "$l_filelist_path"
+}
+
 run_nqiv()
 {
 	# Args
@@ -63,50 +82,64 @@ run_nqiv()
 	local L_EXTENSIONS="$4"
 	local L_FILTER="$5"
 	local L_PASSTHROUGH_ARGS="$6"
+	local L_COMMAND_LIST="$7"
+	local L_QUIT="$8"
 
 	# Create file list
 	local l_filelist_create="mkfifo"
 	local l_filelist_check="check_fifo"
-	if ! command -v mkfifo >/dev/null 2>&1;
+	local l_async="yes"
+	if [ -n "$L_COMMAND_LIST" ] || ! command -v mkfifo >/dev/null 2>&1;
 	then
 		l_filelist_create="touch"
 		l_filelist_check="check_file"
+		l_async=""
 	fi
 
 	local l_filelist_dir="/tmp"
 	local l_filelist_file="nqivf-tmp-files"
 	local l_filelist_file_number="1"
-	if command -v mktemp >/dev/null 2>&1;
+	local l_filelist_path="$L_COMMAND_LIST"
+	if [ -z "$l_filelist_path" ]
 	then
-		l_filelist_dir="$(mktemp -p "$l_filelist_dir" -d .nqivf.XXXXXXXXXX)"
-	else
-		while [ $l_filelist_file_number -le 1000 ] && "$l_filelist_check" "$l_filelist_dir/$l_filelist_file-$l_filelist_file_number";
-		do
-			l_filelist_file_number="$((l_filelist_file_number + 1))"
-		done
-	fi
-
-	local l_filelist_path="$l_filelist_dir/$l_filelist_file-$l_filelist_file_number"
-
-	if [ -e "$l_filelist_path" ] || [ $l_filelist_file_number -gt 1000 ]
-	then
-		errcho "Path of '$l_filelist_path' must not exist, and must be numbered less than or equal to 1000."
-		exit 1
+		if command -v mktemp >/dev/null 2>&1;
+		then
+			l_filelist_dir="$(mktemp -p "$l_filelist_dir" -d .nqivf.XXXXXXXXXX)"
+		else
+			while [ $l_filelist_file_number -le 1000 ] && "$l_filelist_check" "$l_filelist_dir/$l_filelist_file-$l_filelist_file_number";
+			do
+				l_filelist_file_number="$((l_filelist_file_number + 1))"
+			done
+		fi
+		l_filelist_path="$l_filelist_dir/$l_filelist_file-$l_filelist_file_number"
+		if [ -e "$l_filelist_path" ] || [ $l_filelist_file_number -gt 1000 ]
+		then
+			errcho "Path of '$l_filelist_path' must not exist, and must be numbered less than or equal to 1000."
+			exit 1
+		fi
 	fi
 
 	"$l_filelist_create" "$l_filelist_path"
 
 	# Populate file list
-	eval find $L_TARGETS "$L_DEPTH" "$L_EXTENSIONS" "$L_FILTER" -type f -printf "%T@\\\t%f\\\t%p\\\n" \
-	| eval "$L_SORT" \
-	| sed --quiet --posix --sandbox -E -e 's/^.+\t.+\t(.+)$/append image \1/' -e 'p' \
-	> "$l_filelist_path" &
+	if [ -n "$l_async" ]
+	then
+		populate_file "$L_TARGETS" "$L_DEPTH" "$L_EXTENSIONS" "$L_FILTER" "$L_SORT" "$l_filelist_path" &
+	else
+		populate_file "$L_TARGETS" "$L_DEPTH" "$L_EXTENSIONS" "$L_FILTER" "$L_SORT" "$l_filelist_path"
+	fi
 
 	# Actually run nqiv
-	eval nqiv -C "$l_filelist_path" -s "$L_PASSTHROUGH_ARGS"
+	if [ -z "$L_QUIT" ]
+	then
+		eval nqiv -C "$l_filelist_path" -s "$L_PASSTHROUGH_ARGS"
+	fi
 
 	# Cleanup
-	rm "$l_filelist_path"
+	if [ -z "$L_COMMAND_LIST" ]
+	then
+		rm "$l_filelist_path"
+	fi
 	if [ "$l_filelist_dir" != "/tmp" ]
 	then
 		rmdir "$l_filelist_dir"
@@ -163,8 +196,15 @@ main()
 	local a_sort_base="| sort --version-sort --stable --field-separator '	' --key"
 	local a_name_sort_key="2"
 	local a_passthrough_args=""
-	while getopts "he:f:tSRrd:pnBNc:C:" opt;
+	local a_command_list=""
+	local a_quit=""
+	while getopts "he:f:tSRrd:pnBNc:C:l:q" opt;
 	do
+		if [ "$(echo "$OPTARG" | cut -c -1)" = "-" ]
+		then
+			errcho "Argument option '$OPTARG' for '$opt' must not start with a '-'."
+			exit 1
+		fi
 		case "$opt" in
 		'h')
 			exit_usage 0
@@ -197,6 +237,12 @@ main()
 			;;
 		'n')
 			a_sort_base=""
+			;;
+		'l')
+			a_command_list="$OPTARG"
+			;;
+		'q')
+			a_quit="yes"
 			;;
 		'B')
 			a_passthrough_args="$(append_with_space "$a_passthrough_args" "-B")"
@@ -244,7 +290,7 @@ main()
 	fi
 
 	#echo "$l_sort"
-	run_nqiv "$l_targets" "$a_depth" "$l_sort" "$a_extensions" "$a_filter" "$a_passthrough_args"
+	run_nqiv "$l_targets" "$a_depth" "$l_sort" "$a_extensions" "$a_filter" "$a_passthrough_args" "$a_command_list" "$a_quit"
 
 	return 0
 }
