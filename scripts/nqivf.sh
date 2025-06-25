9a072c662ce055c9dff2e2824b7698ca181027ff
buzzingwires <buzzingwires@outlook.com>
2025-06-24 23:30:20 -0400

    nqivf.sh documentation, follow symlinks, sort paths before times.
    * Better describe -d and -r (Override on order and -d default is 1)
    * Follow symlinks by default (consider an extra option not to?)
    * Path sorting is now done through the base sort command.
      This does not override time sorting.

--- scripts/nqivf.sh
+++ scripts/nqivf.sh
@@ -15,9 +15,9 @@ exit_usage()
 	errcho
 	errcho "'-h' : Print this message, then quit."
 	errcho
-	errcho "'-d' : Maximum depth to search for. Overrides -r"
+	errcho "'-d' : Maximum depth to search for. Default is 1. Overrides -r (in order of appearance)."
 	errcho
-	errcho "'-r' : Scan for images recursively with unlimited depth. Overrides -d"
+	errcho "'-r' : Scan for images recursively with unlimited depth. Overrides -d (in order of appearance)."
 	errcho
 	errcho "'-e' : Add extension for nqiv to open."
 	errcho "       Default, case insensitive, containing:"
@@ -65,13 +65,11 @@ populate_file()
 	local L_EXTENSIONS="$3"
 	local L_FILTER="$4"
 	local L_SORT="$5"
-	local L_POST_SORT="$6"
-	local L_FILELIST_PATH="$7"
+	local L_FILELIST_PATH="$6"
 
-	eval find $L_TARGETS "$L_DEPTH" "$L_EXTENSIONS" "$L_FILTER" -type f -printf "%T@//%f//%p\\\0" \
+	eval find -L $L_TARGETS "$L_DEPTH" "$L_EXTENSIONS" "$L_FILTER" -type f -printf "%T@//%f//%p\\\0" \
 	| eval "$L_SORT" \
 	| sed --null-data --quiet --posix --sandbox -E -e 's/\\/\\\\/g' -e 's/\n/\\n/g' -e 's/\r/\\r/g' -e 's/^[0-9]+\.[0-9]+\/\/[^/]+\/\/(\/.+)$/append image \1/' -e 'p' \
-	| eval "$L_POST_SORT" \
 	| tr '\0' '\n' \
 	> "$L_FILELIST_PATH"
 }
@@ -82,12 +80,11 @@ run_nqiv()
 	local L_TARGETS="$1"
 	local L_DEPTH="$2"
 	local L_SORT="$3"
-	local L_POST_SORT="$4"
-	local L_EXTENSIONS="$5"
-	local L_FILTER="$6"
-	local L_PASSTHROUGH_ARGS="$7"
-	local L_COMMAND_LIST="$8"
-	local L_QUIT="$9"
+	local L_EXTENSIONS="$4"
+	local L_FILTER="$5"
+	local L_PASSTHROUGH_ARGS="$6"
+	local L_COMMAND_LIST="$7"
+	local L_QUIT="$8"
 
 	# Create file list
 	local l_filelist_create="mkfifo"
@@ -128,9 +125,9 @@ run_nqiv()
 	# Populate file list
 	if [ -n "$l_async" ]
 	then
-		populate_file "$L_TARGETS" "$L_DEPTH" "$L_EXTENSIONS" "$L_FILTER" "$L_SORT" "$L_POST_SORT" "$l_filelist_path" &
+		populate_file "$L_TARGETS" "$L_DEPTH" "$L_EXTENSIONS" "$L_FILTER" "$L_SORT" "$l_filelist_path" &
 	else
-		populate_file "$L_TARGETS" "$L_DEPTH" "$L_EXTENSIONS" "$L_FILTER" "$L_SORT" "$L_POST_SORT" "$l_filelist_path"
+		populate_file "$L_TARGETS" "$L_DEPTH" "$L_EXTENSIONS" "$L_FILTER" "$L_SORT" "$l_filelist_path"
 	fi
 
 	# Actually run nqiv
@@ -197,8 +194,8 @@ main()
 	local a_reverse_sort=""
 	local a_reverse_reversed_sort="-r"
 	local a_reverse_natural=""
-	local a_sort_base="| sort --zero-terminated --version-sort --stable --field-separator '/' --key 3"
-	local a_post_sort="cat"
+	local a_sort_base="| sort --zero-terminated --version-sort --stable --field-separator '/' --key "
+	local a_sort_base_key="3"
 	local a_passthrough_args=""
 	local a_command_list=""
 	local a_quit=""
@@ -237,7 +234,7 @@ main()
 			a_reverse_natural="| sed --null-data --quiet --posix --sandbox -e '1!G;h;\$!d' -e 'p'"
 			;;
 		'p')
-			a_post_sort="| sort --zero-terminated --version-sort --stable"
+			a_sort_base_key="5"
 			;;
 		'n')
 			a_sort_base=""
@@ -272,6 +269,7 @@ main()
 	if [ -n "$a_sort_base" ]
 	then
 		l_sort="$(append_with_space "$l_sort" "$a_sort_base")"
+		l_sort="$(append_with_space "$l_sort" "$a_sort_base_key")"
 		l_sort="$(append_with_space "$l_sort" "$a_ignore_sort_case")"
 		l_sort="$(append_with_space "$l_sort" "$a_reverse_sort")"
 	else
@@ -284,14 +282,6 @@ main()
 		l_sort="$(append_with_space "$l_sort" "$a_reverse_reversed_sort")"
 	fi
 
-	l_post_sort="cat"
-	if [ -n "$a_post_sort" ]
-	then
-		l_sort="$(append_with_space "$l_post_sort" "$a_post_sort")"
-		l_sort="$(append_with_space "$l_post_sort" "$a_ignore_sort_case")"
-		l_sort="$(append_with_space "$l_post_sort" "$a_reverse_sort")"
-	fi
-
 	a_extensions="$(append_with_space "$a_extensions" "\\)")"
 
 	local l_targets=
@@ -302,7 +292,7 @@ main()
 	fi
 
 	#echo "$l_sort"
-	run_nqiv "$l_targets" "$a_depth" "$l_sort" "$a_post_sort" "$a_extensions" "$a_filter" "$a_passthrough_args" "$a_command_list" "$a_quit"
+	run_nqiv "$l_targets" "$a_depth" "$l_sort" "$a_extensions" "$a_filter" "$a_passthrough_args" "$a_command_list" "$a_quit"
 
 	return 0
 }
