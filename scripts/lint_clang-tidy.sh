02f52d3b35625563e1f4815dc613efae335a9da1
buzzingwires <buzzingwires@outlook.com>
2025-10-28 01:16:17 -0400

    Fix clang-tidy linting infrastructure.
    We now properly include glib so that clang-tidy and define VERSION so
    that clang-tidy can run properly.

--- scripts/lint_clang-tidy.sh
+++ scripts/lint_clang-tidy.sh
@@ -1,3 +1,6 @@
 #!/bin/sh -eu
 
-clang-tidy src/*.c test/*.c src/*.h test/*.h
+clang-tidy src/*.c test/*.c src/*.h test/*.h      \
+		   --                                     \
+		   $(pkg-config glib-2.0 --cflags-only-I) \
+		   -DVERSION=\"CLANGTIDY\"                \
